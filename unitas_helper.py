738f3d40086dc49170e649db8442c1973161aea8
David Wedel <davidwedel06@gmail.com>
2025-08-26 22:07:54 -0500

    add import I forgot

--- unitas_helper.py
+++ unitas_helper.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 import os
 import re
